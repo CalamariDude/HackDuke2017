4737928717dcacbe0e24c4e8ef068aac144078ae
CalamariDude <zeineddine.jad@gmail.com>
2017-10-29 10:12:21 -0400

    added routefinder, not done

--- Server/cyke/routes/index.js
+++ Server/cyke/routes/index.js
@@ -1,10 +1,10 @@
 // server.js
 // call the packages we need
-var express    = require('express');        // call express
+var express = require('express');        // call express
 var router = express.Router();              // get an instance of the express Router
-var app        = express();                 // define our app using express
+var app = express();                 // define our app using express
 var bodyParser = require('body-parser');
-
+var {routefinder} = require("./routefinder.js");
 // configure app to use bodyParser()
 // this will let us get the data from a POST
 app.use(bodyParser.json());
@@ -12,11 +12,23 @@ app.use(bodyParser.json());
 var port = process.env.PORT || 8080;        // set our port
 console.log(port)
 // test route to make sure everything is working (accessed at GET http://localhost:8080/api)
-router.post('/sendDirections', function(req,res){
+
+var sendDirections1 = function(req,res, next){
+console.log(req.body);
+var obj = new routefinder(8);
+res.send(obj.getCSV());
+}
+
+router.post('/sendDirections' , [sendDirections1], function(req,res, next){
      console.log("user attempting to send directions");
      console.log("directions = ", req.body);
-     res.send("thanks!");
-})
+     res.send("thanks, we know where you are!");
+     next()
+          }, function (req, res) {
+               res.send("some type of error");
+     });
+
+
 router.get('/', function(req, res) {
     console.log("here");
     res.json({ message: 'hooray! welcome to our api!' });
---
+++ Server/cyke/routes/routefinder.js
@@ -0,0 +1,71 @@
+var jsts = require('jsts');
+var fs = require("fs");
+
+// Constructor
+class routefinder{
+
+     constructor(carweight) {
+          this.carweight = carweight;
+          this.arr = undefined;
+
+          const csvFilePath='../../north_carolina_bicycle_crash_data_heatmap_.csv';
+          const csv=require('csvtojson')
+          csv()
+          .fromFile(csvFilePath)
+               .on('json',(jsonObj)=>{
+                    arr = jsonObj;
+                    // combine csv header row and csv line to a json object
+                    // jsonObj.a ==> 1 or 4
+               })
+               .on('done',(error)=>{
+                    console.log('end')
+               })
+               console.log(this.arr);
+}
+
+getCSV(){
+     return this.arr;
+}
+
+csvJSON(csv){
+
+  var lines=csv.split("\n");
+  console.log(lines);
+  var result = [];
+
+  var headers=lines[0].split(",");
+
+  for(var i=1;i<lines.length;i++){
+
+	  var obj = {};
+	  var currentline=lines[i].split(",");
+
+	  for(var j=0;j<headers.length;j++){
+		  obj[headers[j]] = currentline[j];
+	  }
+
+	  result.push(obj);
+
+     }
+
+
+  //return result; //JavaScript object
+  return JSON.stringify(result); //JSON
+  }
+
+     // class methods
+     getAllRoutesFromMaps() {
+           var routes = mapsAPI();
+          routes.foreach(function(route) {
+               route.foreach(function(roads){
+                    roads.foreach(function(polygon){
+                         console.log(polygon);
+                    })
+               })
+          } )
+     };
+
+
+}
+// export the class
+exports.routefinder = routefinder;
