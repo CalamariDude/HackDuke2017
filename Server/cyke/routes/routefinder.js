af4a8341c5f477bfd259417b9b596835ec25b11e
timami <timami@ufl.edu>
2017-10-29 11:05:33 -0400

    Removed units distance

--- Server/cyke/routes/routefinder.js
+++ Server/cyke/routes/routefinder.js
@@ -1,5 +1,24 @@
 var jsts = require('jsts');
 var fs = require("fs");
+var inside = require('point-in-polygon');
+
+function distance(lat1, lon1, lat2, lon2) {
+	var radlat1 = Math.PI * lat1/180
+	var radlat2 = Math.PI * lat2/180
+	var theta = lon1-lon2
+	var radtheta = Math.PI * theta/180
+	var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+	dist = Math.acos(dist)
+	dist = dist * 180/Math.PI
+	dist = dist * 60 * 1.1515
+	return dist
+}
+function fatRoad(lat, long, startLat,startLong, stopLat, stopLong) { //Border lat longs
+  var antiSlope = -(stopLat-startLat)/(stopLong-startLong);
+
+  var polygon = [ [Blat1, Blon1], [Blat2, Blon2], [Blat3, Blon3], [Blat4, Blon4] ];
+  return inside([ lat,long ], polygon);
+}
 
 // Constructor
 class routefinder{
